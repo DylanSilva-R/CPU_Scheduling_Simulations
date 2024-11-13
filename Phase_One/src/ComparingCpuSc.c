39b8c0cc1de78ee0faeefbc5418074597b5e8ad4
DylanSilva-R <dylansilvarw@gmail.com>
2024-11-13 17:37:56 -0500

    Yo

---
+++ Phase_One/src/ComparingCpuSc.c
@@ -0,0 +1,177 @@
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Merge.h"
+
+#define MAX_METRIC 20
+#define MIN_METRIC 5
+#define MAX_PROCESSES 10
+#define MIN_PROCESSES 5
+/*
+* TODO:
+* 1) Come up with some constant data to test out functions. Done!
+* 2) Start FCFS. Done
+* 3) Test FCFS. Done
+* 4) Start SJF. Done
+* 5) Test SJF. Done
+* 6) Randomize data. Done
+*/
+
+void calculate_Scheduling(struct Process * pArray, int size, int * totalWt, int * totalTt, int * totalIdlTime) // First-come first-serve function and shortest job first.
+{
+    // Have to include idle time when 
+    int totalTime = 0;
+    int idleTime = 0;
+    int calcTurnaroundTime;
+    int calcWaitTime;
+
+    // Reconfigure, create a while if loop
+
+    int i = 0;
+
+    while(i < size)
+    {
+        if(totalTime >= pArray[i].arrivalTime)
+        {   
+            calcTurnaroundTime = (totalTime + pArray[i].burstTime) - pArray[i].arrivalTime;
+            calcWaitTime = calcTurnaroundTime - pArray[i].burstTime;
+            totalTime += pArray[i].burstTime;
+
+            pArray[i].turnaroundTime = calcTurnaroundTime;
+            pArray[i].waitTime = calcWaitTime;
+
+            *totalWt += calcWaitTime;
+            *totalTt += calcTurnaroundTime;
+
+            i++;
+        }else
+        {
+            if(i ==  0)
+            {
+                idleTime += pArray[i].arrivalTime;
+                totalTime += idleTime;
+            }else
+            {
+                idleTime += abs(totalTime - pArray[i].arrivalTime);
+                totalTime += abs(totalTime - pArray[i].arrivalTime); 
+            }
+
+        }
+    }
+
+    printf("Total time to run all processes: %d\n", totalTime);
+    printf("Total idle time: %d\n", idleTime);
+
+    *totalIdlTime += idleTime;
+}
+
+
+void print_Processes(struct Process * pArray, int size)
+{
+    // Implement a readjustable table.
+    printf("______________________________\n");
+    printf("| Pi | A.T | B.T | W.T | T.T |\n");
+    for(int i = 0; i < size; i++)
+    {
+        printf("| %s | %d | %d | %d | %d |\n", pArray[i].ProcessID, pArray[i].arrivalTime, pArray[i].burstTime, pArray[i].waitTime, pArray[i].turnaroundTime);
+    }
+    printf("______________________________\n");
+}
+
+
+void create_Processes(struct Process pArray[], int processes)
+{
+
+    int arrivalTime;
+    int burstTime;
+
+    for(int i = 0; i < processes; i++)
+    {
+        arrivalTime = rand() % (MAX_METRIC - MIN_METRIC + 1) + MIN_METRIC;
+        burstTime = rand() % (MAX_METRIC - MIN_METRIC + 1) + MIN_METRIC;
+
+        char processesID[10];
+        snprintf(pArray[i].ProcessID, sizeof(pArray[i].ProcessID), "P%d", i); 
+
+        pArray[i].arrivalTime = arrivalTime;
+        pArray[i].burstTime = burstTime;
+        pArray[i].turnaroundTime = 0;
+        pArray[i].waitTime = 0;
+
+    }
+
+    //print_Processes(pArray, processes);
+}
+
+
+int main()
+{
+    // Create random data for both CPU scheduling algorithms and collect data.
+    // testFCFS();
+    // testSJF();
+    
+    clock_t start = clock();
+    srand(time(NULL));
+
+    int iterations = 100;
+    int totalProcesses = 0;
+
+    int totalWtFCFS = 0; // Total waittime for FCFS
+    int totalTtFCFS = 0; // Total turnaround time for FCFS
+    int totalIdlTimeFCFS = 0; // Total idle time for FCFS.
+
+    int totalWTSJF = 0;
+    int totalTtSJF = 0;
+    int totalIdlTimeSJF = 0;
+
+    for(int i = 0; i < iterations; i++)
+    {
+        printf("Iteration %d\n", i);
+
+        int processes = rand() % (MAX_PROCESSES - MIN_PROCESSES + 1) + MIN_PROCESSES;
+        totalProcesses += processes;
+        struct Process pArray[processes];
+
+        create_Processes(pArray, processes);
+
+        mergeSort(pArray, 0, processes-1, 0); //FCFS
+        calculate_Scheduling(pArray, processes, &totalWtFCFS, &totalTtFCFS, &totalIdlTimeFCFS);
+        print_Processes(pArray, processes);
+
+
+        mergeSort(pArray, 0, processes-1, 1); // SJF
+        calculate_Scheduling(pArray, processes, &totalWTSJF, &totalTtSJF, &totalIdlTimeSJF);
+        print_Processes(pArray, processes);
+
+    }
+
+    float avgWtFCFS = (float)totalWtFCFS / totalProcesses;
+    float avgTtFCFS = (float)totalTtFCFS / totalProcesses;
+    float avgIdlTimeFCFS = (float)totalIdlTimeFCFS / iterations;
+
+    float avgWtSJF = (float)totalWTSJF / totalProcesses;
+    float avgTtSJF = (float)totalTtSJF / totalProcesses;
+    float avgIdlTimeSJF = (float)totalIdlTimeSJF / iterations;
+
+    printf("\n");
+    printf("Averages for %d iterations of FCFS for %d processes\n", iterations, totalProcesses);
+    printf("Avg wait: %.2f\n", avgWtFCFS);
+    printf("Avg T.T: %.2f\n", avgTtFCFS);
+    printf("Avg idle: %.2f\n", avgIdlTimeFCFS);
+    printf("\n");
+
+    printf("\n");
+    printf("Averages for %d iterations of SJF for %d processes\n", iterations, totalProcesses);
+    printf("Avg wait: %.2f\n", avgWtSJF);
+    printf("Avg T.T: %.2f\n", avgTtSJF);
+    printf("Avg idle: %.2f\n", avgIdlTimeSJF);
+    printf("\n");
+
+
+    clock_t end = clock();
+
+    printf("Run time: %.2fs\n", (float)(end - start)/CLOCKS_PER_SEC);
+
+    return 1;
+}
---
+++ Phase_Two/src/CPUDeadlock.c
@@ -0,0 +1,542 @@
+
+/*
+ * TODO:
+ * 1) Implement deadlock conditions
+ *   1.1) Mutual exclusion
+ *   1.2) Hold and wait
+ *   1.3) No preemption
+ *   1.4) Circular wait
+ * 2) Implement deadlock detection: Banker's algorithm
+ * 3) Simulate deadlock by implementing deadlock prone code.
+ * 4) Deadlock handling: Banker's algorithm
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "Merge.h"
+
+#define RESOURCE_SIZE 5
+#define BILLION 1000000000L;
+
+pthread_mutex_t lock;
+
+struct Process_With_R // Struct to define processes with resources
+{
+    /*
+    * Will need to readjust this struct. Need to implement dynamic memory allocation.
+    */
+    char processID[50];
+    int resourceAllocation[RESOURCE_SIZE];
+    int resourceRequestMax[RESOURCE_SIZE];
+    int resourceAvailable[RESOURCE_SIZE];
+    int resourceNeed[RESOURCE_SIZE];
+    int processRan; // Only 0 or 1 symbolizing true or false.
+    int safeSequenceIndex[RESOURCE_SIZE];
+    int runTime;
+    // int SizeOfPArray;
+    // int resourceCount;
+    // int waitTime;
+    // int turnAroundTime;
+};
+
+void *process_Action(void *arg) // Process performing an action. Process threads will use this function.
+{
+    /*
+     * - This function has mutex implemented to unlock and lock resources.
+     * -
+     */
+
+    struct Process_With_R *process = (struct Process_With_R *)arg;
+
+    pthread_mutex_lock(&lock);
+
+    printf("Process %s is running.\n", process->processID);
+    printf("Process run time is %d\n", process->runTime);
+    sleep(process->runTime);
+
+    pthread_mutex_unlock(&lock);
+    free(process);
+    pthread_exit(NULL);
+}
+
+void print_Bankers_Algo(struct Process_With_R *pArray, int sizeOfPArray, int resourceCount)
+{
+    for (int i = 0; i < sizeOfPArray; i++)
+    {
+        printf("%s: ", pArray[i].processID);
+
+        printf("|");
+        for (int j = 0; j < resourceCount; j++)
+        {
+            printf("%d ", pArray[i].resourceAllocation[j]);
+        }
+        printf("|");
+
+        for (int j = 0; j < resourceCount; j++)
+        {
+            printf("%d ", pArray[i].resourceRequestMax[j]);
+        }
+        printf("|");
+
+        for (int j = 0; j < resourceCount; j++)
+        {
+            printf("%d ", pArray[i].resourceAvailable[j]);
+        }
+        printf("|");
+
+        for (int j = 0; j < resourceCount; j++)
+        {
+            printf("%d ", pArray[i].resourceNeed[j]);
+        }
+        printf("|");
+
+        printf("\n");
+    }
+
+    printf("Safe sequence: ");
+    for (int i = 0; i < sizeOfPArray - 1; i++)
+    {
+
+        if (i < sizeOfPArray - 2)
+        {
+            printf("%s => ", pArray[pArray[0].safeSequenceIndex[i]].processID);
+        }
+        else
+        {
+            printf("%s", pArray[pArray[0].safeSequenceIndex[i]].processID);
+        }
+    }
+    printf("\n");
+}
+
+int bankers_Algo_Avoidance(struct Process_With_R *pArray, int sizeOfPArray, int resourceCount)
+{
+    /*
+     * Purpose of function:
+     *   - Calculate the safe sequence using Bankers Algorithm
+     *   - Return safe sequence error code. 1 safe sequence was sucessfully created, 0 safe sequence was not created.
+     */
+
+    int resourceAllocationTotals[resourceCount];
+    int total;
+    int calcNeed;
+    int successfully_Calculated = 0;
+
+    // First, calculate total for all process allocations.
+
+    for (int i = 0; i < resourceCount; i++)
+    {
+        total = 0;
+
+        for (int j = 0; j < sizeOfPArray; j++)
+        {
+            total += pArray[j].resourceAllocation[i];
+        }
+
+        resourceAllocationTotals[i] = total;
+    }
+
+    // Second, calculate need matrix.
+
+    for (int i = 0; i < sizeOfPArray; i++)
+    {
+        for (int j = 0; j < resourceCount; j++)
+        {
+            calcNeed = pArray[i].resourceRequestMax[j] - pArray[i].resourceAllocation[j];
+            pArray[i].resourceNeed[j] = calcNeed;
+        }
+    }
+
+    /*
+     * Third, find the safe sequence
+     */
+
+    for (int i = 0; i < sizeOfPArray; i++)
+    {
+        for (int j = 0; j < sizeOfPArray; j++)
+        {
+            int countValidRes = 0;
+            int holdAllocationRes;
+            int holdAvailableRes;
+            int holdNeedRes;
+
+            if (pArray[j].processRan == 0)
+            {
+                for (int k = 0; k < resourceCount; k++)
+                {
+                    holdAvailableRes = pArray[i].resourceAvailable[k];
+                    holdNeedRes = pArray[j].resourceNeed[k];
+
+                    if (holdNeedRes <= holdAvailableRes)
+                    {
+                        countValidRes++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (countValidRes == resourceCount)
+            {
+
+                if (i < sizeOfPArray)
+                {
+                    pArray[0].safeSequenceIndex[i] = j;
+                    // safeSequenceIndex[i] = j;
+                    pArray[j].processRan = 1;
+
+                    for (int k = 0; k < resourceCount; k++)
+                    {
+                        holdAllocationRes = pArray[j].resourceAllocation[k];
+                        holdAvailableRes = pArray[i].resourceAvailable[k];
+
+                        pArray[i + 1].resourceAvailable[k] = holdAllocationRes + holdAvailableRes;
+                    }
+                }
+                successfully_Calculated++;
+                break;
+            }
+        }
+    }
+
+    if (successfully_Calculated == sizeOfPArray)
+    {
+        return 1; // Safe sequence exists
+    }
+    else
+    {
+        return 0; // There is no safe sequence
+    }
+}
+
+void run_Threads(struct Process_With_R *pArray, int sizeOfPArray)
+{
+    // Last element is there to calculate the last available matrix value.
+
+    int safeSequenceIndex[sizeOfPArray];
+
+    pthread_t processes[sizeOfPArray];
+    pthread_mutex_init(&lock, NULL);
+
+    // Create threads.
+
+    for (int i = 0; i < sizeOfPArray; i++)
+    {
+        struct Process_With_R *hold = (struct Process_With_R *)malloc(sizeof(struct Process_With_R));
+
+        if (hold == NULL)
+        {
+            perror("Memory allocation failed.\n");
+        }
+
+        *hold = pArray[pArray->safeSequenceIndex[i]];
+
+        if (pthread_create(processes + i, NULL, &process_Action, (void *)hold) != 0)
+        {
+            printf("Error creating thread.\n");
+            free(hold);
+        }
+
+        printf("Process %d started.\n", i);
+    }
+
+    // Join threads.
+
+    for (int i = 0; i < sizeOfPArray; i++)
+    {
+        if (pthread_join(processes[i], NULL) != 0)
+        {
+            printf("Error joining threads.\n");
+        }
+
+        printf("Process %d finished runnning.\n", i);
+    }
+
+    // Destroy mutex lock.
+    pthread_mutex_destroy(&lock);
+}
+
+int isInteger(char *input)
+{
+    // Strip leading and trailing whitespaces
+    while (*input == ' ' || *input == '\t')
+    {
+        input++;
+    }
+
+    // If input is empty after trimming, return 0 (false)
+    if (*input == '\0')
+    {
+        return 0;
+    }
+
+    // Check for an optional sign ('+' or '-')
+    if (*input == '+' || *input == '-')
+    {
+        input++;
+    }
+
+    // Check if all characters are digits
+    while (*input)
+    {
+        if (!isdigit(*input))
+        {
+            return 0; // Not an integer if there's a non-digit character
+        }
+        input++;
+    }
+
+    // Input is a valid integer if we've only seen digits (and possibly a sign)
+    return 1;
+}
+
+int validate_Integer_Input()
+{
+    /*
+     * Take in user input and check if its an integer or not.
+     */
+    int true = 1;
+    int num;
+
+    while (true)
+    {
+        num = 0;
+
+        char buffer[100];
+
+        if (fgets(buffer, sizeof(buffer), stdin) != NULL)
+        {
+            buffer[strcspn(buffer, "\n")] = '\0';
+
+            if (isInteger(buffer))
+            {
+                num = atoi(buffer);
+                true = 0;
+            }
+            else
+            {
+                printf("Invalid input. Please try again.\n");
+            }
+        }
+        else
+        {
+            printf("Error reading input.\n");
+            continue;
+        }
+    }
+
+    return num;
+}
+
+void menu()
+{
+    printf("1) Low processes\n");
+    printf("2) Medium processes\n");
+    printf("3) High processes\n");
+    printf("4) EXIT\n");
+}
+
+int main()
+{
+    /*
+     *   Main Function Structure
+     *   _______________________
+     *
+     *   - Create processes array.
+     *   - Create Safe sequence array
+     *   - Calculate safe sequence using
+     *
+     *
+     */
+
+    printf(" _______________________\n");
+    printf("|CPU Deadlock Simulation|\n");
+    printf("|_______________________|\n");
+    printf("By: Dylan Silva-Rivas\n");
+    printf("\n");
+
+    // Small amount of processes
+    struct Process_With_R pArraySmall[] = {{"P0", {0, 0, 1, 2, 1}, {0, 0, 1, 2, 5}, {1, 5, 2, 0, 2}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}}, // Process, allocation, max resource request, resource need
+                                           {"P1", {1, 0, 0, 0, 3}, {1, 7, 5, 0, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P2", {1, 3, 5, 4, 2}, {2, 3, 5, 6, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P3", {0, 6, 3, 2, 5}, {0, 6, 5, 2, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P4", {0, 0, 1, 4, 6}, {0, 6, 5, 6, 10}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P5", {0, 1, 2, 9, 6}, {1, 2, 3, 10, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"Empty", {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}}};
+    // Last element is there to calculate the last available matrix value.
+
+    // Medium amount of processes
+    struct Process_With_R pArrayMed[] = {{"P0", {0, 0, 1, 2, 1}, {0, 0, 1, 2, 5}, {1, 5, 2, 0, 2}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}}, // Process, allocation, max resource request, resource need
+                                         {"P1", {1, 0, 0, 0, 3}, {1, 7, 5, 0, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P2", {1, 3, 5, 4, 2}, {2, 3, 5, 6, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P3", {0, 6, 3, 2, 5}, {0, 6, 5, 2, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P4", {0, 0, 1, 4, 6}, {0, 6, 5, 6, 10}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P5", {0, 2, 2, 9, 6}, {1, 3, 4, 9, 8}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P6", {0, 3, 3, 10, 7}, {1, 4, 3, 10, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P7", {0, 5, 4, 3, 8}, {2, 6, 6, 8, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P8", {0, 2, 3, 2, 3}, {5, 5, 5, 6, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P9", {0, 3, 5, 1, 2}, {6, 4, 6, 9, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"P10", {0, 2, 2, 9, 6}, {1, 3, 3, 11, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                         {"Empty", {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}}};
+    // Last element is there to calculate the last available matrix value.
+
+    // Large amount of processes
+    struct Process_With_R pArrayLarge[] = {{"P0", {0, 0, 1, 2, 1}, {0, 0, 1, 2, 5}, {1, 5, 2, 0, 2}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}}, // Process, allocation, max resource request, resource need
+                                           {"P1", {1, 0, 0, 0, 3}, {1, 7, 5, 0, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P2", {1, 3, 5, 4, 2}, {2, 3, 5, 6, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P3", {0, 6, 3, 2, 5}, {0, 6, 5, 2, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P4", {0, 0, 1, 4, 6}, {0, 6, 5, 6, 10}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           
+                                           {"P5", {2, 4, 3, 8, 6}, {3, 5, 3, 9, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P6", {5, 2, 4, 5, 7}, {6, 3, 5, 6, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P7", {5, 2, 2, 4, 8}, {11, 5, 6, 4, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P8", {6, 6, 2, 5, 9}, {7, 8, 3, 6, 10}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P9", {1, 5, 2, 8, 1}, {2, 6, 5, 9, 10}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P10", {2, 8, 2, 5, 2}, {5, 9, 4, 6, 5}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P11", {3, 9, 2, 2, 2}, {4, 10, 3, 4, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P12", {1, 10, 2, 1, 3}, {1, 11, 4, 4, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P13", {2, 3, 2, 6, 4}, {3, 5, 3, 8, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P14", {7, 2, 2, 7, 5}, {8, 3, 4, 8, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P15", {9, 1, 2, 8, 7}, {12, 2, 5, 9, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P16", {8, 2, 2, 2, 5}, {9, 2, 3, 4, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P17", {6, 5, 2, 5, 6}, {7, 9, 5, 5, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P18", {5, 7, 2, 9, 8}, {9, 7, 3, 10, 12}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P19", {3, 6, 2, 1, 7}, {12, 10, 15, 13, 9}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"P20", {1, 5, 2, 4, 2}, {20, 22, 24, 25, 29}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}},
+                                           {"Empty", {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, 0, {0, 0, 0, 0, 0}}};
+
+    int loop = 1;
+    struct timespec res;
+    long start;
+    long end;
+    double runTime;
+    int sizeOfPArray;
+    int resourceCount;
+    int safeSequenceCode;
+
+    while (loop)
+    {
+        menu();
+
+        printf("Enter menu option: ");
+        int menuInput = validate_Integer_Input();
+        printf("\n");
+
+        switch (menuInput)
+        {
+            case 1:
+                sizeOfPArray = (sizeof(pArraySmall) / sizeof(pArraySmall[0]));             // Size of process array.
+                resourceCount = (sizeof(pArraySmall[1].resourceAllocation) / sizeof(int)); // Size of resources.
+
+                printf("Amount of processes: %d\n", sizeOfPArray);
+                printf("Amount of resources: %d\n", resourceCount);
+
+                if(clock_gettime(CLOCK_REALTIME,&res) == -1)
+                {
+                    perror("Couldn't obtain time.\n");
+                    return EXIT_FAILURE;
+                }
+
+                start = res.tv_nsec;
+
+                safeSequenceCode = bankers_Algo_Avoidance(pArraySmall, sizeOfPArray, resourceCount);
+                
+                if(clock_gettime(CLOCK_REALTIME,&res) == -1)
+                {
+                    perror("Couldn't obtain time.\n");
+                    return EXIT_FAILURE;
+                }
+
+                end = res.tv_nsec;
+                runTime = end - start;
+
+                print_Bankers_Algo(pArraySmall, sizeOfPArray, resourceCount);
+                printf("Banker's algorithm runtime: %lfns\n", runTime);
+
+                run_Threads(pArraySmall, sizeOfPArray-1);
+            
+                break;
+            case 2:
+                sizeOfPArray = (sizeof(pArrayMed) / sizeof(pArrayMed[0]));               // Size of process array.
+                resourceCount = (sizeof(pArrayMed[1].resourceAllocation) / sizeof(int)); // Size of resources.
+
+                printf("Amount of processes: %d\n", sizeOfPArray);
+                printf("Amount of resources: %d\n", resourceCount);
+
+                if(clock_gettime(CLOCK_REALTIME,&res) == -1)
+                {
+                    perror("Couldn't obtain time.\n");
+                    return EXIT_FAILURE;
+                }
+
+                start = res.tv_nsec;
+
+                safeSequenceCode = bankers_Algo_Avoidance(pArrayMed, sizeOfPArray, resourceCount);
+                
+                if(clock_gettime(CLOCK_REALTIME,&res) == -1)
+                {
+                    perror("Couldn't obtain time.\n");
+                    return EXIT_FAILURE;
+                }
+
+                end = res.tv_nsec;
+                runTime = end - start;
+
+                print_Bankers_Algo(pArrayMed, sizeOfPArray, resourceCount);
+                printf("Banker's algorithm runtime: %lfns\n", runTime);
+
+                run_Threads(pArrayMed, sizeOfPArray-1);
+
+                break;
+            case 3:
+                sizeOfPArray = (sizeof(pArrayLarge) / sizeof(pArrayLarge[0]));             // Size of process array.
+                resourceCount = (sizeof(pArrayLarge[1].resourceAllocation) / sizeof(int)); // Size of resources.
+
+                printf("Amount of processes: %d\n", sizeOfPArray);
+                printf("Amount of resources: %d\n", resourceCount);
+
+                if(clock_gettime(CLOCK_REALTIME,&res) == -1)
+                {
+                    perror("Couldn't obtain time.\n");
+                    return EXIT_FAILURE;
+                }
+
+                start = res.tv_nsec;
+
+                safeSequenceCode = bankers_Algo_Avoidance(pArrayLarge, sizeOfPArray, resourceCount);
+                
+                if(clock_gettime(CLOCK_REALTIME,&res) == -1)
+                {
+                    perror("Couldn't obtain time.\n");
+                    return EXIT_FAILURE;
+                }
+
+                end = res.tv_nsec;
+                runTime = end - start;
+
+                print_Bankers_Algo(pArrayLarge, sizeOfPArray, resourceCount);
+                printf("Banker's algorithm runtime: %lfns\n", runTime);
+
+                run_Threads(pArrayLarge, sizeOfPArray-1);
+                
+                break;
+            case 4:
+                loop = 0;
+                break;
+            default:
+                printf("Your input is out of bounds. Please try again.\n");
+                break;
+                continue;
+        }
+    }
+
+    return 1;
+}
